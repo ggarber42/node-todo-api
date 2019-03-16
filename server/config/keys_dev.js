dbc0379d2c5bea199d773c4f083241a04c85d0be
Guilherme Garber <ggarber@protonmail.com>
2019-03-16 17:37:06 -0300

    Added keys for cloud Atlas DB

---
+++ server/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://new-me_42:IpjxrOfaaSt89qkP@cluster0-kvvg8.mongodb.net/test?retryWrites=true',
+    secretOrKey: 'YOUR_OWN_SECRET'
+  };
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
+const db = require('.././config/keys').mongoURI;
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
 
 module.exports = {mongoose};
